53193acf4c519e8c5ffdfb7db79fbefb6735b16b
ruarrimactire <huracanster@gmail.com>
2014-08-07 22:06:19 +0200

    queen behaviour correction

--- tests/pieces/QueenTest.java
+++ tests/pieces/QueenTest.java
@@ -21,11 +21,22 @@ public class QueenTest extends TestCase {
         Board board = new Board();
         Queen queen = new Queen(Colour.WHITE);
         board.setPosition("a1", queen);
+        
+        board.setPosition("a5", new Queen(Colour.BLACK)); // attenzione non deve scavalcare
+        
+//        System.out.println("Test regina: ");
+//        System.out.println(board.toString());
         ArrayList<String> moves = queen.getPossibleMoves(board);
+//        { int i=0;
+//        while(i < moves.size()){
+//            System.out.println(moves.get(i));
+//            i++;
+//        }}
 
         assertFalse(moves.contains("a1"));
         assertTrue(moves.contains("h1"));
-        assertTrue(moves.contains("a8"));
+        assertTrue(moves.contains("a5"));
+        assertFalse(moves.contains("a8"));
         assertTrue(moves.contains("b2"));
         assertTrue(moves.contains("h8"));
     }
